b1eee3d2ca70e7ef56421d16b1e30fd42ac8db4a
AleSha <alesha@MacBook-Air-Admin.local>
2022-06-30 14:26:20 +0300

    До переноса на сервер

--- js/script.js
+++ js/script.js
@@ -267,5 +267,22 @@ document.addEventListener('DOMContentLoaded', () => {
         'menu__item'
     ).render();
 
+
+
+    const http = require('http');
+
+    const hostname = '127.0.0.1';
+    const port = 3000;
+    
+    const server = http.createServer((req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Hello World');
+    });
+    
+    server.listen(port, hostname, () => {
+      console.log(`Server running at http://${hostname}:${port}/`);
+    });
+
 });
 
